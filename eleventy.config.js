88c44d5f70e6887750ad28eae699dec1a27bd5e6
Sam Buchmiller <sam.buchmiller@gmail.com>
2024-02-21 19:44:03 -0700

    Add better styling

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,9 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPassthroughCopy({
+    "./water.min.css": "/water.min.css"
+});
   return {
     pathPrefix: "/blog/",
   };
